a929c82b8aa5652fb3dd57cb5513490da5e9ce74
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-06-10 14:31:43 -0300

    delete

--- src/src/router/auth.router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controller/auth.controller");
-
-router.post("/login", authController.loginController);
-
-module.exports = router;
